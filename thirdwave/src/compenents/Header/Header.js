544beb9d7a2148fb32147c2ed76f74b2aedee4ed
Gürkan <gurkanyigitgr@gmail.com>
2022-11-15 18:39:18 +0300

    modified header

--- thirdwave/src/compenents/Header/Header.js
+++ thirdwave/src/compenents/Header/Header.js
@@ -3,38 +3,29 @@ import { NavLink } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
+
+import "./style.css";
 
 export default function Header() {
   return (
-    <Navbar bg="light" expand="lg">
-      <Container>
-        <Navbar.Brand as={NavLink} to="/home">
-          3rd Wave
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link as={NavLink} to="/home">
-              Home
-            </Nav.Link>
-            <Nav.Link as={NavLink} to="/about">
-              About
-            </Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <div className="navbar">
+      <Nav.Link as={NavLink} to="/">
+        3rd Wave
+      </Nav.Link>
+
+      <div className="navItems">
+        <Nav.Link as={NavLink} to="/" className="menuItems">
+          Home
+        </Nav.Link>
+
+        <Nav.Link as={NavLink} to="/about" className="menuItems">
+          About
+        </Nav.Link>
+
+        <Nav.Link as={NavLink} to="/contact" className="menuItems">
+          Contact
+        </Nav.Link>
+      </div>
+    </div>
   );
 }
