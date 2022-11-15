d1da6de3558b1a2c5ce09e25f258455e89f69f3d
Gürkan <gurkanyigitgr@gmail.com>
2022-11-15 17:01:41 +0300

    add router

--- thirdwave/src/App.js
+++ thirdwave/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Header from "./compenents/Header/Header";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Routes>
+        <Route element={<Home />} path="/home" />
+        <Route element={<About />} path="/about" />
+      </Routes>
     </div>
   );
 }
