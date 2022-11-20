2427867cecbb25a92c00a282c1c53abe378df4da
Gürkan <gurkanyigitgr@gmail.com>
2022-11-20 11:38:17 +0300

    added sass

--- thirdwave/src/pages/Home/Home.js
+++ thirdwave/src/pages/Home/Home.js
@@ -1,6 +1,9 @@
 import React from "react";
 import "../../sass/pages/_home.scss";
+import "../../sass/components/_accordion.scss";
 import Header from "../../compenents/Header/Header";
+import { accordionData } from "../../utils/content";
+import Accordion from "../../compenents/Accordion/Accordion";
 
 export default function Home() {
   const clickMe = () => {
@@ -10,20 +13,43 @@ export default function Home() {
   return (
     <div>
       <Header />
-      <div class="left-bg" />
-      <div className="right-bg">
-        <div className="main-info">
-          <h1>
-            Welcome To <br />
-            Your Goal In <br />
-            <span className="life-header">Real</span> Life.
+
+      <div id="home">
+        <div className="left-bg" />
+        <div className="right-bg">
+          <div className="main-info">
+            <h1>
+              Welcome To <br />
+              Your Goal In <br />
+              <span className="life-header">Real</span> Life.
+              <br />
+            </h1>
             <br />
-          </h1>
-          <br />
-          <span className="wave-header" onClick={() => clickMe()}>
-            3rd Wave
-          </span>
+            <span className="wave-header" onClick={() => clickMe()}>
+              3rd Wave
+            </span>
+          </div>
         </div>
+      </div>
+
+      <div className="section-a">
+        <h1>
+          If you want to enjoy moments
+          <br /> with passion and persistence, <br />
+          you are in right place.
+        </h1>
+
+        <div className="accordion">
+          {accordionData.map(({ title, content }) => (
+            <Accordion title={title} content={content} />
+          ))}
+        </div>
+      </div>
+      <div className="section-b">
+        <h1>
+          If you want to enjoy moments with passion and persistence, you are in
+          right place.
+        </h1>
       </div>
     </div>
   );
