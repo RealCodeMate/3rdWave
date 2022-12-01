5834b1d9229c10643bafb0bb7c7da0f47d04847b
bayogluteoman <bayogluteoman@gmail.com>
2022-12-01 03:22:26 +0300

    add about page content

--- thirdwave/src/pages/About/About.js
+++ thirdwave/src/pages/About/About.js
@@ -1,13 +1,24 @@
 import React from "react";
 import Header from "../../compenents/Header/Header";
+import "../../sass/pages/_about.scss";
+import image from "../../assets/aboutMe.jpeg"
+import Footer from "../../compenents/Footer/Footer";
 
 export default function About() {
   return (
-    <div>
-     <Header />
-     <div> 
-      <h1>This is an about page about doctor</h1>
-     </div>
+    <div id="parent">
+      <Header />
+      <div className="about">
+        <div className="left-about">
+          <h1>So who am I ? </h1>
+          <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+          <a>Contact me</a>
+        </div>
+        <div className="right-about">
+          <img src={image} alt="img" className="aboutmeImage" />
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 }
