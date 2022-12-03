cb2cee0fe3d67ec0e344a5d90bcb159d94d9867e
bayogluteoman <bayogluteoman@gmail.com>
2022-12-03 20:02:53 +0300

    use param for treatments

--- thirdwave/src/App.js
+++ thirdwave/src/App.js
@@ -2,7 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
 import Contact from "./pages/Contact/Contact";
-import Tedaviler from "./pages/Tedaviler/tedaviler";
+import Treatments from "./pages/Treatments/Treatments";
 import "./App.css";
 import "./App.scss";
 
@@ -13,7 +13,8 @@ function App() {
         <Route element={<Home />} path="/" />
         <Route element={<About />} path="/about" />
         <Route element={<Contact />} path="/contact" />
-        <Route element={<Tedaviler />} path="/tedaviler" />
+        <Route element={<Treatments />} path="/treatments" />
+        <Route element={<Treatments />} path="/treatments/:string" />
       </Routes>
     </div>
   );
