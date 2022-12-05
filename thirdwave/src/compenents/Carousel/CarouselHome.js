bf37affaeb1ff9a609be5d624cda754511b91d25
bayogluteoman <bayogluteoman@gmail.com>
2022-12-05 23:38:39 +0300

    decrease image number

--- thirdwave/src/compenents/Carousel/CarouselHome.js
+++ thirdwave/src/compenents/Carousel/CarouselHome.js
@@ -33,69 +33,7 @@ function IndividualIntervalsExample() {
         <Carousel.Caption className="carosuel-content">
         </Carousel.Caption>
       </Carousel.Item>
-      <Carousel.Item interval={5000}>
-        <img
-          className="d-block w-50"
-          src={messi}
-          alt="First slide"
-        />
-        <Carousel.Caption className="carosuel-content">
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item interval={5000}>
-        <img
-          className="d-block w-50"
-          src={messi}
-          alt="First slide"
-        />
-        <Carousel.Caption className="carosuel-content">
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item interval={5000}>
-        <img
-          className="d-block w-50"
-          src={messi}
-          alt="First slide"
-        />
-        <Carousel.Caption className="carosuel-content">
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item interval={5000}>
-        <img
-          className="d-block w-50"
-          src={messi}
-          alt="First slide"
-        />
-        <Carousel.Caption className="carosuel-content">
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item interval={5000}>
-        <img
-          className="d-block w-50"
-          src={messi}
-          alt="First slide"
-        />
-        <Carousel.Caption className="carosuel-content">
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item interval={5000}>
-        <img
-          className="d-block w-50"
-          src={messi}
-          alt="Second slide"
-        />
-        <Carousel.Caption className="carosuel-content">
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item interval={5000}>
-        <img
-          className="d-block w-50"
-          src={messi}
-          alt="Third slide"
-        />
-        <Carousel.Caption className="carosuel-content">
-        </Carousel.Caption>
-      </Carousel.Item>
+      
     </Carousel>
   );
 }
