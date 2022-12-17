9e8085a0b241cee24e341f500b2d9671e2f8fbfc
bayogluteoman <bayogluteoman@gmail.com>
2022-12-17 23:09:56 +0300

    add png for flags

--- thirdwave/src/compenents/lngBtn.js
+++ thirdwave/src/compenents/lngBtn.js
@@ -3,11 +3,13 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 import "../sass/components/_header.scss";
 import { useTranslation } from 'react-i18next';
 import "../sass/components/_header.scss";
+import trFlag from "../assets/turkish.png";
+import enFlag from "../assets/english.png";
 
 
 export default function LngBtn() {
     const [open, setOpen] = useState(false);
-    const [language, setLanguage] = useState('tr') 
+    const [language, setLanguage] = useState('tr')
     const { i18n } = useTranslation();
     const { t } = useTranslation();
     const handleOpen = () => {
@@ -33,17 +35,29 @@ export default function LngBtn() {
     return (
         <div className="dropdown">
             <button onClick={handleOpen}>
-                 {language==='tr' ?  <button>{t('header.language.tr')}</button> : <button>{t('header.language.en')} </button>}
-                
+                {language === 'tr' ?
+                    <div className="menu-item">
+                        <button>{t('header.language.tr')}</button>
+                        <img src={trFlag} style={{ marginLeft:'5px', height: '20px', width: '20px' }}></img>
+                    </div>
+                    :
+                    <div className="menu-item">
+                        <button>{t('header.language.en')}</button>
+                        <img src={enFlag} style={{ marginLeft:'5px', height: '20px', width: '20px' }}></img>
+                    </div>
+                }
+
+
             </button>
             {open ? (
                 <ul className="menu">
                     <li className="menu-item">
-                    <button onClick={handleLanguageTr}>{t('header.language.tr')}</button>
+                        <button onClick={handleLanguageTr}>{t('header.language.tr')}</button>
+                        <img src={trFlag} style={{ height: '20px', width: '20px' }}></img>
                     </li>
                     <li className="menu-item">
-                    <button onClick={handleLanguageEn}>{t('header.language.en')} </button>
-
+                        <button onClick={handleLanguageEn}>{t('header.language.en')} </button>
+                        <img src={enFlag} style={{ height: '20px', width: '20px' }}></img>
                     </li>
                 </ul>
             ) : null}
