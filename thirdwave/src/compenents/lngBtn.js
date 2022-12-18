5d116178579c43f6266fff41dcda0e058c9f5815
bayogluteoman <bayogluteoman@gmail.com>
2022-12-18 22:31:28 +0300

    add russian language support

--- thirdwave/src/compenents/lngBtn.js
+++ thirdwave/src/compenents/lngBtn.js
@@ -5,6 +5,7 @@ import { useTranslation } from 'react-i18next';
 import "../sass/components/_header.scss";
 import trFlag from "../assets/turkish.png";
 import enFlag from "../assets/english.png";
+import ruFlag from "../assets/russia.png";
 
 
 export default function LngBtn() {
@@ -15,9 +16,11 @@ export default function LngBtn() {
     const handleOpen = () => {
         setOpen(!open);
     }
+    
 
     const changeLanguage = (lng) => {
         i18n.changeLanguage(lng);
+        console.log(language)
     };
 
     const handleLanguageTr = () => {
@@ -32,32 +35,59 @@ export default function LngBtn() {
         setOpen(false);
     }
 
+    const handleLanguageRu = () => {
+        changeLanguage('ru')
+        setLanguage('ru')
+        setOpen(false);
+    }
+
+    const handleLanguages = () => {
+        if (language === 'tr') {
+            return (
+                <div className="menu-item">
+                    <button>{t('header.language.tr')}</button>
+                    <img src={trFlag} style={{ marginLeft: '5px', height: '20px', width: '20px' }}></img>
+                </div>
+            )
+        }
+        if (language === 'en') {
+            return (
+                <div className="menu-item">
+                    <button>{t('header.language.en')}</button>
+                    <img src={enFlag} style={{ marginLeft: '5px', height: '20px', width: '20px' }}></img>
+                </div>
+            )
+        }
+        if (language === 'ru') {
+            return (
+                <div className="menu-item">
+                    <button>{t('header.language.ru')}</button>
+                    <img src={ruFlag} style={{ marginLeft: '5px', height: '20px', width: '20px' }}></img>
+                </div>
+            )
+        }
+    }
+
     return (
         <div className="dropdown">
             <button onClick={handleOpen}>
-                {language === 'tr' ?
-                    <div className="menu-item">
-                        <button>{t('header.language.tr')}</button>
-                        <img src={trFlag} style={{ marginLeft:'5px', height: '20px', width: '20px' }}></img>
-                    </div>
-                    :
-                    <div className="menu-item">
-                        <button>{t('header.language.en')}</button>
-                        <img src={enFlag} style={{ marginLeft:'5px', height: '20px', width: '20px' }}></img>
-                    </div>
-                }
+                {handleLanguages()}
 
 
             </button>
             {open ? (
                 <ul className="menu">
-                    <li className="menu-item">
-                        <button onClick={handleLanguageTr}>{t('header.language.tr')}</button>
+                    <li className="menu-item" onClick={handleLanguageTr} key='1'>
+                        <button >{t('header.language.tr')}</button>
                         <img src={trFlag} style={{ height: '20px', width: '20px' }}></img>
                     </li>
-                    <li className="menu-item">
-                        <button onClick={handleLanguageEn}>{t('header.language.en')} </button>
+                    <li className="menu-item" onClick={handleLanguageEn} key='2'>
+                        <button >{t('header.language.en')} </button>
                         <img src={enFlag} style={{ height: '20px', width: '20px' }}></img>
+                    </li>
+                    <li className="menu-item" onClick={handleLanguageRu} key='3'>
+                        <button >{t('header.language.ru')} </button>
+                        <img src={ruFlag} style={{ height: '20px', width: '20px' }}></img>
                     </li>
                 </ul>
             ) : null}
