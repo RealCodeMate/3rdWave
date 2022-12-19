6c0450fec7fb3fc3037d1185e3482942abed8d38
bayogluteoman <bayogluteoman@gmail.com>
2022-12-20 01:41:29 +0300

    add data array for Q&A

--- thirdwave/src/compenents/Accordion/AllQA.js
+++ thirdwave/src/compenents/Accordion/AllQA.js
@@ -3,59 +3,30 @@ import { useTranslation } from "react-i18next";
 
 function MyAccordion() {
     const { t } = useTranslation();
+
+    const data = [
+        { key: '0', question: `${t("faqPage.questions.q1")}`, answer: `${t("faqPage.answers.a1")}` },
+        { key: '1', question: `${t("faqPage.questions.q2")}`, answer: `${t("faqPage.answers.a2")}` },
+        { key: '2', question: `${t("faqPage.questions.q3")}`, answer: `${t("faqPage.answers.a3")}` },
+        { key: '3', question: `${t("faqPage.questions.q4")}`, answer: `${t("faqPage.answers.a4")}` },
+        { key: '4', question: `${t("faqPage.questions.q5")}`, answer: `${t("faqPage.answers.a5")}` },
+        { key: '5', question: `${t("faqPage.questions.q6")}`, answer: `${t("faqPage.answers.a6")}` },
+        { key: '6', question: `${t("faqPage.questions.q7")}`, answer: `${t("faqPage.answers.a7")}` },
+        { key: '7', question: `${t("faqPage.questions.q8")}`, answer: `${t("faqPage.answers.a8")}` },
+        { key: '8', question: `${t("faqPage.questions.q9")}`, answer: `${t("faqPage.answers.a9")}` },
+        { key: '9', question: `${t("faqPage.questions.q10")}`, answer: `${t("faqPage.answers.a10")}` },
+    ]
+
     return (
         <Accordion defaultActiveKey="0" flush>
-
-            <Accordion.Item eventKey="0">
-                <Accordion.Header>{t("faqPage.questions.q1")}</Accordion.Header>
-                <Accordion.Body>{t("faqPage.answers.a1")}</Accordion.Body>
-            </Accordion.Item>
-
-            <Accordion.Item eventKey="1">
-                <Accordion.Header>{t("faqPage.questions.q2")}</Accordion.Header>
-                <Accordion.Body>{t("faqPage.answers.a2")}</Accordion.Body>
-            </Accordion.Item>
-
-            <Accordion.Item eventKey="2">
-                <Accordion.Header>{t("faqPage.questions.q3")}</Accordion.Header>
-                <Accordion.Body>{t("faqPage.answers.a3")}</Accordion.Body>
-            </Accordion.Item>
-
-            <Accordion.Item eventKey="3">
-                <Accordion.Header>{t("faqPage.questions.q4")}</Accordion.Header>
-                <Accordion.Body>{t("faqPage.answers.a4")}</Accordion.Body>
-            </Accordion.Item>
-
-            <Accordion.Item eventKey="4">
-                <Accordion.Header>{t("faqPage.questions.q5")}</Accordion.Header>
-                <Accordion.Body>{t("faqPage.answers.a5")}</Accordion.Body>
-            </Accordion.Item>
-
-            <Accordion.Item eventKey="5">
-                <Accordion.Header>{t("faqPage.questions.q6")}</Accordion.Header>
-                <Accordion.Body>{t("faqPage.answers.a6")}</Accordion.Body>
-            </Accordion.Item>
-
-            <Accordion.Item eventKey="6">
-                <Accordion.Header>{t("faqPage.questions.q7")}</Accordion.Header>
-                <Accordion.Body>{t("faqPage.answers.a7")}</Accordion.Body>
-            </Accordion.Item>
-
-            <Accordion.Item eventKey="7">
-                <Accordion.Header>{t("faqPage.questions.q8")}</Accordion.Header>
-                <Accordion.Body>{t("faqPage.answers.a8")}</Accordion.Body>
-            </Accordion.Item>
-
-            <Accordion.Item eventKey="8">
-                <Accordion.Header>{t("faqPage.questions.q9")}</Accordion.Header>
-                <Accordion.Body>{t("faqPage.answers.a9")}</Accordion.Body>
-            </Accordion.Item>
-
-            <Accordion.Item eventKey="9">
-                <Accordion.Header>{t("faqPage.questions.q10")}</Accordion.Header>
-                <Accordion.Body>{t("faqPage.answers.a10")}</Accordion.Body>
-            </Accordion.Item>
-
+            {data.map(el => {
+                return (
+                    <Accordion.Item eventKey={el.key}>
+                        <Accordion.Header>{el.question}</Accordion.Header>
+                        <Accordion.Body>{el.answer}</Accordion.Body>
+                    </Accordion.Item>
+                )
+            })}
         </Accordion>
     );
 }
--- thirdwave/src/compenents/Accordion/MyAccordion.js
+++ thirdwave/src/compenents/Accordion/MyAccordion.js
@@ -4,36 +4,23 @@ import { useTranslation } from 'react-i18next';
 function MyAccordion() {
   const { t } = useTranslation();
 
+  const data = [
+    { key: '0', question: `${t("homePage.faqSection.accordion.questions.q1")}`, answer: `${t("homePage.faqSection.accordion.answers.a1")}` },
+    { key: '1', question: `${t("homePage.faqSection.accordion.questions.q2")}`, answer: `${t("homePage.faqSection.accordion.answers.a2")}` },
+    { key: '2', question: `${t("homePage.faqSection.accordion.questions.q3")}`, answer: `${t("homePage.faqSection.accordion.answers.a3")}` },
+    { key: '3', question: `${t("homePage.faqSection.accordion.questions.q4")}`, answer: `${t("homePage.faqSection.accordion.answers.a4")}` },
+  ]
+
   return (
     <Accordion defaultActiveKey="0" flush>
-      <Accordion.Item eventKey="0">
-        <Accordion.Header>
-          {t('homePage.faqSection.accordion.questions.q1')}
-        </Accordion.Header>
-        <Accordion.Body>
-        {t('homePage.faqSection.accordion.answers.a1')}
-        </Accordion.Body>
-      </Accordion.Item>
-      <Accordion.Item eventKey="1">
-        <Accordion.Header>
-        {t('homePage.faqSection.accordion.questions.q2')}
-        </Accordion.Header>
-        <Accordion.Body>
-        {t('homePage.faqSection.accordion.answers.a2')}
-        </Accordion.Body>
-      </Accordion.Item>
-      <Accordion.Item eventKey="2">
-        <Accordion.Header>{t('homePage.faqSection.accordion.questions.q3')}</Accordion.Header>
-        <Accordion.Body>
-        {t('homePage.faqSection.accordion.answers.a3')}
-        </Accordion.Body>
-      </Accordion.Item>
-      <Accordion.Item eventKey="3">
-        <Accordion.Header>{t('homePage.faqSection.accordion.questions.q4')}</Accordion.Header>
-        <Accordion.Body>
-        {t('homePage.faqSection.accordion.answers.a4')}
-        </Accordion.Body>
-      </Accordion.Item>
+      {data.map(el => {
+        return (
+          <Accordion.Item eventKey={el.key}>
+            <Accordion.Header>{el.question}</Accordion.Header>
+            <Accordion.Body>{el.answer}</Accordion.Body>
+          </Accordion.Item>
+        )
+      })}
     </Accordion>
   );
 }
